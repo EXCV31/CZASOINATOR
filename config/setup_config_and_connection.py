7721c2bb566d7df930a754705480990aff6ace69
Filip Laszczak <filip.laszczak.1997@gmail.com>
2022-12-13 23:28:36 +0100

    Added some assertions to config

--- config/setup_config_and_connection.py
+++ config/setup_config_and_connection.py
@@ -1,6 +1,7 @@
 import configparser
 import logging
 from helpers.colors import get_color
+from helpers.exit_handler import exit_program
 from redminelib import Redmine
 
 logging.basicConfig(filename='czasoinator.log', encoding='utf-8', level=logging.DEBUG, format='[%(asctime)s] %('
@@ -20,7 +21,22 @@ def parse_config():
     logging.info("Parsowanie config.ini...")
     config = configparser.ConfigParser()
     config.read("config/config.ini")
-    redmine_conf = config['REDMINE']
+
+    # Check if config.ini is valid.
+    try:
+        redmine_conf = config['REDMINE']
+        redmine_conf['ADDRESS']
+        redmine_conf['API_KEY']
+        redmine_conf['DAILY']
+        redmine_conf['EXCLUDE']
+        redmine_conf['OFF_DAYS_PER_YEAR']
+    except KeyError:
+        logging.error("Błąd parsowania config.ini")
+
+        # BaseException instead helpers/error_handler, because frame_title is not initialized.
+        raise BaseException("Plik konfiguracyjny jest uszkodzony lub nie został poprawnie wypełniony.")
+        exit_program(1)
+
     logging.info("Parsowanie ukończone.")
 
     # Setup frame title: display <orange>CZASOINATOR <white>- <red>{split address from config}
